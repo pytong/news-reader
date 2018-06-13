4bab901f5029c61d542940042f94d8f9b9f051e4
Puiyee Tong <pytong07@gmail.com>
2018-06-12 23:43:01 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        news-reader
+      </h1>
+      <ul>
+        <li v-for="(article, index) in articles" :key="index">
+          <span>{{ article }}</span>
+        </li>
+        <li><input placeholder="What needs to be done?" @keyup.enter="addTodo"></li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Logo from '~/components/Logo.vue'
+
+export default {
+  computed: mapGetters({
+    articles: 'articles/get'
+  }),
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title
+{
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+.subtitle
+{
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.links
+{
+  padding-top: 15px;
+}
+</style>
