f1ed6e0c16ac3b45faeae0a5e18e6fd974fde89b
Puiyee Tong <pytong07@gmail.com>
2018-06-13 23:30:45 -0400

    Slugify news article url

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,10 @@
       </h1>
       <b-row v-for="(chunk, rowIndex) in articleChunks(4)" :key="rowIndex">
         <b-col cols="3" v-for="(article, colIndex) in chunk" :key="colIndex">
-          <span>{{ article.title }}</span>
-          <img class="articleImage" :src ="article.urlToImage" />
+          <nuxt-link :to="{ name: 'articles-slug', params: { slug: urlSlug(article.title) }}">
+            {{ article.title }}
+            <img class="articleImage" :src ="article.urlToImage" />
+          </nuxt-link>
         </b-col>
       </b-row>
     </div>
@@ -16,6 +18,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import slugify from 'slugify'
 
 export default {
   computed: mapGetters({
@@ -25,6 +28,9 @@ export default {
   methods: {
     articleChunks(size) {
       return _.chunk(this.articles, size)
+    },
+    urlSlug(url) {
+      return slugify(url)
     }
   },
 
