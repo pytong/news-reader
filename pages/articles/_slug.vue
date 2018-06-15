37483efb9a3f1b33876cf48ff7cda2c66deac228
Puiyee Tong <pytong07@gmail.com>
2018-06-15 01:45:38 -0400

    Fetch news article

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -18,7 +18,7 @@ export default {
   methods: {
     async fetchArticle () {
       const url = 'http://localhost:3001/news'
-      const content = await this.$axios.$get(url, {
+      const { content } = await this.$axios.$get(url, {
         params: {
           articleUrl: this.$route.query.articleUrl
         }
@@ -27,16 +27,9 @@ export default {
       })
 
       console.log(content)
-      // this.$store.commit('setViewingArticleScreenshot', content)
     },
     generateViewArticleScreenshot() {
-      // const pageres = new Pageres({delay: 2})
-      //   .src('yeoman.io', ['480x320', '1024x768', 'iphone 5s'], {crop: true})
-      //   .src('todomvc.com', ['1280x1024', '1920x1080'])
-      //   .src('data:text/html;base64,PGgxPkZPTzwvaDE+', ['1024x768'])
-      //   .dest(__dirname)
-      //   .run()
-      //   .then(() => console.log('done'));
+
     }
   },
 
